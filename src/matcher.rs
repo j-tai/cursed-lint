959ab98c7926bab6e83bb8349ab69493b757fa10
Jasmine Tai <jtai@jtai.ca>
2023-09-11 23:25:50 -0400

    Implement wildcard token matching

--- src/matcher.rs
+++ src/matcher.rs
@@ -47,15 +47,125 @@ pub fn find_matches<'a>(tokens: &'a [Token<'a>]) -> Vec<Warning<'a>> {
                 if pattern.len() > target.len() {
                     continue;
                 }
-                if (0..pattern.len()).all(|j| target[j] == pattern[j] || pattern[j] == "_") {
+                if let Some(matched_tokens) = check_match(tokens, pattern) {
                     // Matched here
                     warnings.push(Warning {
                         description: rule.name,
-                        tokens: target,
+                        tokens: matched_tokens,
                     })
                 }
             }
         }
     }
     warnings
+}
+
+/// Checks if the given token sequence matches the given pattern. Returns the matched portion.
+pub fn check_match<'a>(
+    tokens: &'a [Token<'a>],
+    pattern: &'a [Token<'a>],
+) -> Option<&'a [Token<'a>]> {
+    let mut index = 0;
+    let mut pattern = pattern.iter().peekable();
+    while let Some(expected) = pattern.next() {
+        match expected.value {
+            "_" => {
+                // Match an identifier or keyword
+                if index >= tokens.len() {
+                    return None;
+                }
+                let token = &tokens[index];
+                index += 1;
+                if !matches!(token.value.chars().next().unwrap(), 'a'..='z' | 'A'..='Z' | '_') {
+                    return None;
+                }
+            }
+            "__" => {
+                // Match any token
+                if index >= tokens.len() {
+                    return None;
+                }
+                index += 1;
+            }
+            "___" => {
+                // Match any sequence of tokens
+                let ending = *pattern.peek().expect("cannot end pattern with ___");
+                while tokens.get(index) != Some(&ending) {
+                    if index >= tokens.len() {
+                        return None;
+                    }
+                    index += 1;
+                }
+            }
+            _ => {
+                // Match the token
+                if index >= tokens.len() {
+                    return None;
+                }
+                if &tokens[index] != expected {
+                    return None;
+                }
+                index += 1;
+            }
+        }
+    }
+
+    Some(&tokens[..index])
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::lexer::lex;
+    use crate::matcher::check_match;
+
+    #[test]
+    fn test_literal() {
+        assert_eq!(
+            check_match(&lex("foo\n\tbar\t3.14   baz   a"), &lex("foo bar 3.14 baz"),),
+            Some(lex("foo bar 3.14 baz").as_slice()),
+        )
+    }
+
+    #[test]
+    fn test_any_identifier() {
+        assert_eq!(
+            check_match(&lex("foo\n\tbar\t3.14   baz   a"), &lex("foo _ 3.14 baz"),),
+            Some(lex("foo bar 3.14 baz").as_slice()),
+        )
+    }
+
+    #[test]
+    fn test_any_identifier_not_matching() {
+        assert_eq!(
+            check_match(&lex("foo\n\tbar\t3.14   baz   a"), &lex("foo bar _ baz"),),
+            None,
+        )
+    }
+
+    #[test]
+    fn test_any_token() {
+        assert_eq!(
+            check_match(&lex("foo\n\tbar\t3.14   baz   a"), &lex("foo bar __ baz"),),
+            Some(lex("foo bar 3.14 baz").as_slice()),
+        )
+    }
+
+    #[test]
+    fn test_any_token_sequence() {
+        assert_eq!(
+            check_match(&lex("foo\n\tbar\t3.14   baz   a"), &lex("foo ___ baz"),),
+            Some(lex("foo bar 3.14 baz").as_slice()),
+        )
+    }
+
+    #[test]
+    fn test_any_token_sequence_empty() {
+        assert_eq!(
+            check_match(
+                &lex("foo\n\tbar\t3.14   baz   a"),
+                &lex("foo bar ___ 3.14 baz"),
+            ),
+            Some(lex("foo bar 3.14 baz").as_slice()),
+        )
+    }
 }
